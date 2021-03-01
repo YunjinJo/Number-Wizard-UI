270248829ca43a1e259dd295d2f3d67511be6f7d
YunjinJo <black44jo@naver.com>
2021-03-01 23:30:50 +0900

    Added NumberWizard script

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max = 1000;
+    [SerializeField] int min = 1;
+    int guess = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+    void StartGame()
+    {
+        max = max + 1;
+        guess = (max + min) / 2;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
